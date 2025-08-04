ca9c089505fac44f30bb3078d6b79c513d6962a0
Jack Christie <christiej3@autonation.com>
2025-08-04 14:11:29 -0400

    fix

--- chat_uikit/lib/src/message_composer/cometchat_message_composer.dart
+++ chat_uikit/lib/src/message_composer/cometchat_message_composer.dart
@@ -324,6 +324,7 @@ class _CometChatMessageComposerState extends State<CometChatMessageComposer> {
       hideCollaborativeWhiteboardOption:
           widget.hideCollaborativeWhiteboardOption,
       hideTakePhotoOption: widget.hideTakePhotoOption,
+      disableMentions: widget.disableMentions,
     );
 
     super.didChangeDependencies();
@@ -358,15 +359,16 @@ class _CometChatMessageComposerState extends State<CometChatMessageComposer> {
                 value.textEditingController!.text,
               );
 
-      final shouldUseBackgroundColor =
-          isTextControllerEmpty || isSameAsOldMessage || isEditModeWithoutChanges;
+      final shouldUseBackgroundColor = isTextControllerEmpty ||
+          isSameAsOldMessage ||
+          isEditModeWithoutChanges;
 
       return Container(
         decoration: BoxDecoration(
-            color: messageComposerStyle.sendButtonIconBackgroundColor ??
-                (shouldUseBackgroundColor
-                    ? colorPalette.background4
-                    : colorPalette.primary),
+          color: messageComposerStyle.sendButtonIconBackgroundColor ??
+              (shouldUseBackgroundColor
+                  ? colorPalette.background4
+                  : colorPalette.primary),
           borderRadius: messageComposerStyle.sendButtonBorderRadius ??
               BorderRadius.circular(spacing.radiusMax ?? 0),
         ),
